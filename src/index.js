1a3d9b76d0b46076cb9e2261c8a8b90b247128fb
Umidullo Suyunov <suyunovumidullo@gmail.com>
2020-10-25 01:16:07 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
-module.exports = function toReadable (number) {
-  const obj = {
-    0: 'zero',
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-    6: 'six',
-    7: 'seven',
-    8: 'eight',
-    9: 'nine',
-    10: 'ten',
-    11: 'eleven',
-    12: 'twelve',
-    13: 'thirteen',
-  }
+const a = [
+  "",
+  "one ",
+  "two ",
+  "three ",
+  "four ",
+  "five ",
+  "six ",
+  "seven ",
+  "eight ",
+  "nine ",
+  "ten ",
+  "eleven ",
+  "twelve ",
+  "thirteen ",
+  "fourteen ",
+  "fifteen ",
+  "sixteen ",
+  "seventeen ",
+  "eighteen ",
+  "nineteen ",
+];
+const b = [
+  "",
+  "",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+];
 
-  number.toString().split('');
-  for (let i = number.length; i > 0; i--) {
-    for( let j = 0; j < 14; j++) {
-      if(j == nunber[i]) {
-        number[i] = obj['i']
-      }
-    }
-  }
-  return number;
-}
+module.exports = function toReadable(number) {
+  if (number == 0) return "zero";
+
+  let n = ("000" + number).substr(-3).match(/^(\d{1})(\d{2})$/);
+
+  if (!n) return;
+
+  let str = "";
+
+  str +=
+      n[1] != 0
+          ? (a[Number(n[1])] || b[n[1][0]] + " " + a[n[1][1]]) + "hundred "
+          : "";
+  str += n[2] != 0 ? a[Number(n[2])] || b[n[2][0]] + " " + a[n[2][1]] : "";
+
+  console.log(str);
+
+  return str.replace(/\s+$/, "");
+};
